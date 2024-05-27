348e5054de66851f06abdeb6568824a693e9c473
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-05-27 19:08:12 +0530

    add commit write methods

--- regression-finder/src/main/java/com/tool/CSVWriter.java
+++ regression-finder/src/main/java/com/tool/CSVWriter.java
@@ -3,34 +3,45 @@ package com.tool;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import com.tool.templates.GitCommit;
 import com.tool.templates.TestResult;
 
 public class CSVWriter implements ResultsWriter {
 
     private BufferedWriter writer;
 
-    CSVWriter(String filePath,BufferedWriter writer){
+    CSVWriter(String filePath, BufferedWriter writer) {
         this.writer = writer;
     }
 
     public void close() throws IOException {
-        
+
         writer.close();
     }
 
-    public static CSVWriter createNewWriter(String filePath) throws IOException{
+    public static CSVWriter createNewWriter(String filePath) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
         return new CSVWriter(filePath, bufferedWriter);
     }
 
-    public void writeTestResult(TestResult testResult) {
-        try{
-        writer.append(testResult.getTestClass() + "," + testResult.getTestMethod() + "," + testResult.getResult() + "\n");
-        writer.flush();
-        }
-        catch(IOException e){
+    @Override
+    public void writeLine(String line){
+        try {
+            writer.append(line);
+            writer.flush();
+        } catch (IOException e) {
             System.out.println(e.getStackTrace());
         }
     }
-}
 
+    @Override
+    public void writeTestResult(TestResult testResult) {
+        writeLine(testResult.getTestClass() + "," + testResult.getTestMethod() + "," + testResult.getResult() + "\n");
+    }
+
+    @Override
+    public void writeCommit(GitCommit gitCommit) {
+        writeLine(gitCommit.commitId + "," + gitCommit.parentCommitId + "," + gitCommit.authorMail + "," + gitCommit.branch + "\n");  
+    }
+}
--- regression-finder/src/main/java/com/tool/ResultsWriter.java
+++ regression-finder/src/main/java/com/tool/ResultsWriter.java
@@ -1,8 +1,13 @@
 package com.tool;
 
+import com.tool.templates.GitCommit;
 import com.tool.templates.TestResult;
 
 public interface ResultsWriter {
 
+    public void writeLine(String line);
+
     public void writeTestResult(TestResult testResult);
+
+    public void writeCommit(GitCommit gitCommit);
 }
