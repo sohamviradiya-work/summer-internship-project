1c6d67267db6993d60eda8f3efd80a5075512845
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-05-28 17:55:54 +0530

    add regression blame template

---
+++ regression-finder/src/main/java/com/tool/templates/RegressionBlame.java
@@ -0,0 +1,21 @@
+package com.tool.templates;
+
+import com.tool.templates.TestResult.TestIndentifier;
+
+public class RegressionBlame implements CSVItem{
+    private String author;
+    private String commit;
+    private String testClass;
+    private String testMethod;
+    public RegressionBlame(TestIndentifier testIndentifier, GitCommit gitCommit) {
+        this.author = gitCommit.getAuthor();
+        this.commit = gitCommit.getCommitId();
+        this.testClass = testIndentifier.getTestClass();
+        this.testMethod = testIndentifier.getTestMethod();
+    }
+    
+    @Override
+    public String toCSVString() {
+        return testClass + "," + testMethod + "," + commit + "," + author + "\n"; 
+    }
+}
