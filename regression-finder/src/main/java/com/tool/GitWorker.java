c0b874ec30227e0e78da15d3b01722a67dcbebef
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-05-28 15:49:41 +0530

    fetch all remote branches

--- regression-finder/src/main/java/com/tool/GitWorker.java
+++ regression-finder/src/main/java/com/tool/GitWorker.java
@@ -1,9 +1,11 @@
 package com.tool;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -26,11 +28,11 @@ public class GitWorker {
         this.revWalk = new RevWalk(repository);
     }
 
-    public Repository getRepository(){
+    public Repository getRepository() {
         return this.repository;
     }
 
-    public void close(){
+    public void close() {
         revWalk.close();
     }
 
@@ -57,47 +59,51 @@ public class GitWorker {
         return null;
     }
 
-    public void checkoutToCommit(String commitTag){
+    public void checkoutToCommit(String commitTag) {
         try (Git git = new Git(repository)) {
-                RevCommit commit = revWalk.parseCommit(repository.resolve(commitTag));
-                git.checkout().setName(commit.getName()).call();
-                System.out.println("Checked out to commit: " + commit.getName());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            RevCommit commit = revWalk.parseCommit(repository.resolve(commitTag));
+            git.checkout().setName(commit.getName()).call();
+            System.out.println("Checked out to commit: " + commit.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-public void listCommits(ItemWriter<GitCommit> commitsWriter) throws IOException {
-    try (Git git = new Git(repository)) {
-        List<Ref> branches = git.branchList().call();
-        for (Ref branch : branches) {
-            String branchName = branch.getName();
 
-            Iterable<RevCommit> commits = git.log().add(repository.resolve(branchName)).call();
+    public void listCommits(ItemWriter<GitCommit> commitsWriter) throws IOException {
+        try (Git git = new Git(repository)) {
             
-            for (RevCommit commit : commits) {
-                String parentId;
-                if (commit.getParentCount() > 0) {
-                    RevCommit parent = commit.getParent(0);
-                    parentId = parent.getName();
-                } else {
-                    parentId = "HEAD";
+            List<Ref> branches = git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call();
+
+            for (Ref branch : branches) {
+                String branchName = branch.getName();
+                
+                ObjectId branchObjectId = repository.resolve(branchName);
+                if (branchObjectId == null) {
+                    continue;
                 }
 
-                commitsWriter.write(new GitCommit(
-                        commit.getAuthorIdent().getEmailAddress(),
-                        commit.getName(),
-                        parentId,
-                        branchName,
-                        commit.getAuthorIdent().getWhen(),
-                        commit.getShortMessage()
-                ));
+                Iterable<RevCommit> commits = git.log().add(branchObjectId).call();
+
+                for (RevCommit commit : commits) {
+                    String parentId;
+                    if (commit.getParentCount() > 0) {
+                        RevCommit parent = commit.getParent(0);
+                        parentId = parent.getName();
+                    } else {
+                        parentId = "HEAD";
+                    }
+
+                    commitsWriter.write(new GitCommit(
+                            commit.getAuthorIdent().getEmailAddress(),
+                            commit.getName(),
+                            parentId,
+                            branchName,
+                            commit.getAuthorIdent().getWhen(),
+                            commit.getShortMessage()));
+                }
             }
+        } catch (GitAPIException e) {
+            e.printStackTrace();
         }
-    } catch (GitAPIException e) {
-        e.printStackTrace();
     }
-}
-
-
-
 }
