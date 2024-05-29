6ea8a1b578f983f29578afa20b6dac2510c1d44c
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-05-29 11:32:34 +0530

    add branch to regression blame

--- regression-finder/src/main/java/com/tool/templates/RegressionBlame.java
+++ regression-finder/src/main/java/com/tool/templates/RegressionBlame.java
@@ -5,10 +5,12 @@ import com.tool.templates.TestResult.TestIndentifier;
 public class RegressionBlame implements CSVItem{
     private String author;
     private String commit;
+    private String branch;
     private String testClass;
     private String testMethod;
     public RegressionBlame(TestIndentifier testIndentifier, GitCommit gitCommit) {
         this.author = gitCommit.getAuthor();
+        this.branch = gitCommit.getBranch();
         this.commit = gitCommit.getCommitId();
         this.testClass = testIndentifier.getTestClass();
         this.testMethod = testIndentifier.getTestMethod();
@@ -16,6 +18,6 @@ public class RegressionBlame implements CSVItem{
     
     @Override
     public String toCSVString() {
-        return testClass + "," + testMethod + "," + commit + "," + author; 
+        return testClass + "," + branch +  "," + testMethod + "," + commit + "," + author; 
     }
 }
