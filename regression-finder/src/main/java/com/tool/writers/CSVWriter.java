7d1f8929c150d97637b4676b80be83b0a298da1e
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-05-29 18:37:41 +0530

    move flush to close function

--- regression-finder/src/main/java/com/tool/writers/CSVWriter.java
+++ regression-finder/src/main/java/com/tool/writers/CSVWriter.java
@@ -17,7 +17,6 @@ public class CSVWriter<T extends CSVItem> implements ItemWriter<T> {
     public void write(T item) throws IOException {
         writer.write(item.toCSVString());
         writer.newLine();
-        writer.flush();
     }
 
     public static <T extends CSVItem> CSVWriter<T> create(String filePath) throws IOException {
@@ -27,6 +26,7 @@ public class CSVWriter<T extends CSVItem> implements ItemWriter<T> {
 
     public void close() throws IOException {
         if (writer != null) {
+            writer.flush();
             writer.close();
         }
     }
