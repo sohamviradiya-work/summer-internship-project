2e94bac286757e495008ad191686d36ed485e842
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-04 11:31:52 +0530

    add file writing methods

--- repo-generator/src/main/java/com/initializers/TestClassWriter.java
+++ repo-generator/src/main/java/com/initializers/TestClassWriter.java
@@ -1,5 +1,9 @@
 package com.initializers;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.utils.Helper;
 
@@ -20,7 +24,7 @@ public class TestClassWriter {
         TestMethodWriter[] testMethodWriters = new TestMethodWriter[numOfMethods];
 
         for (int i = 0; i < numOfMethods; i++) {
-            testMethodWriters[i] = TestMethodWriter.writeTestMethod(i,randomCeiling);
+            testMethodWriters[i] = TestMethodWriter.writeTestMethod(i, randomCeiling);
             classDeclaration.addMember(testMethodWriters[i].methodDeclaration);
         }
         return new TestClassWriter(classDeclaration, testMethodWriters);
@@ -28,6 +32,22 @@ public class TestClassWriter {
 
     public void modifyTestClass(int methodNum, int x, int y) {
         testMethodWriters[methodNum].modifyTestMethod(x, y);
+    }
+
+    public void writeToFile(String modulePath, String packageName) throws IOException {
+
+        CompilationUnit compilationUnit = new CompilationUnit();
+
+        compilationUnit.setPackageDeclaration(packageName);
+        compilationUnit.addType(classDeclaration);
+
+        compilationUnit.addImport("org.junit.jupiter.api.Test");
+        compilationUnit.addImport("org.junit.jupiter.api.Assertions.assertEquals");
+
+        String outputFilePath = modulePath + "/" + classDeclaration.getNameAsString() + ".java";
+        FileOutputStream output = new FileOutputStream(outputFilePath);
+        output.write(compilationUnit.toString().getBytes());
+        output.close();
     }
 
 }
