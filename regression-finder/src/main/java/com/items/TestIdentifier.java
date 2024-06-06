354f63af853cebfd725c3171ac5c1d4176b0a259
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-06 14:08:11 +0530

    remove colon from project name

--- regression-finder/src/main/java/com/items/TestIdentifier.java
+++ regression-finder/src/main/java/com/items/TestIdentifier.java
@@ -75,6 +75,6 @@ public final class TestIdentifier implements Comparable<TestIdentifier>, CSVItem
 
     @Override
     public String toCSVString() {
-        return testProject + "," + testClass + "," + testMethod;
+        return testProject.substring(1) + "," + testClass + "," + testMethod;
     }
 }
