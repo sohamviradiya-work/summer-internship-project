41f67e564cce496b15e3b8d9dc5e1c55d21c8e0c
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-07 12:16:18 +0530

    clean main

--- regression-finder/src/main/java/com/tool/Main.java
+++ regression-finder/src/main/java/com/tool/Main.java
@@ -1,23 +1,8 @@
 package com.tool;
 
 import java.io.File;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Map.Entry;
-
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.NoHeadException;
-
-import com.items.GitCommit;
-import com.items.RegressionBlame;
-import com.items.TestIdentifier;
-import com.items.TestResult;
 import com.tool.git.GitWorker;
-import com.tool.writers.CSVWriter;
 
 public class Main {
     public static String repositoryPath = "../test-area/repository";
@@ -30,50 +15,9 @@ public class Main {
 
         try {
             GitWorker.getRemoteRepository(repositoryPath, repositoryLink);
-            run();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    private static void run() throws IOException, NoHeadException, GitAPIException {
-
-        TargetProject targetProject = TargetProject.mountLocalProject(repositoryPath, "7.6.4");
-
-        writeTestResults(targetProject);
-
-        writecommits(targetProject);
-
-        CSVWriter<RegressionBlame> blameCSVWriter = CSVWriter.create(resultsPath + "blame-tests.csv");
-
-        blameCSVWriter.write(new RegressionBlame(new TestIdentifier(":testProject","testClass", "testMethod"),
-                new GitCommit("author", "commit", "parent", "branch", Date.from(Instant.now()), "message")));
-
-        targetProject.runFailedTestsBranchWise(blameCSVWriter);
-        blameCSVWriter.close();
-        targetProject.close();
-    }
-
-    private static void writecommits(TargetProject targetProject) throws IOException, NoHeadException, GitAPIException {
-        CSVWriter<GitCommit> commitsCSVWriter = CSVWriter.create(resultsPath + "commits-list.csv");
-        commitsCSVWriter.write(new GitCommit("author", "commit", "parent", "branch", Date.from(Instant.now()), "message"));
-        HashMap<String, ArrayList<GitCommit>> branchCommitMap = targetProject.getGitWorker().listCommitsByBranch();
-        for (Entry<String, ArrayList<GitCommit>> entry : branchCommitMap.entrySet()) {
-            for (GitCommit gitCommit : entry.getValue()) {
-                commitsCSVWriter.write(gitCommit);
-            }
-        }
-        commitsCSVWriter.close();
-    }
-
-    private static void writeTestResults(TargetProject targetProject) throws IOException {
-        ArrayList<TestResult> testResults = targetProject.getRunner().runAllTests();
-        CSVWriter<TestResult> testResultsCSVWriter = CSVWriter.create(resultsPath + "failed-tests.csv");
-
-        for(TestResult testResult:testResults){
-            testResultsCSVWriter.write(testResult);
-        }
-        testResultsCSVWriter.close();
     }
 
     @SuppressWarnings("unused")
