0e08517340eff70ed8f07fe2ae925cb04429fbb2
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-10 11:42:19 +0530

    add batch regression finder

---
+++ regression-finder/src/main/java/com/tool/finders/BatchRegressionFinder.java
@@ -0,0 +1,62 @@
+package com.tool.finders;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import com.items.ProjectCommit;
+import com.items.RegressionBlame;
+import com.items.TestIdentifier;
+import com.items.TestResult;
+import com.tool.ProjectInstance;
+import com.tool.writers.interfaces.ItemWriter;
+
+public class BatchRegressionFinder extends LinearRegressionFinder {
+
+    private int batchSize;
+
+    public BatchRegressionFinder(ProjectInstance projectInstance, ItemWriter<RegressionBlame> blameWriter,
+            int batchSize) {
+        super(projectInstance, blameWriter);
+        this.batchSize = batchSize;
+    }
+
+    @Override
+    public void runForCommitsAndTests(ArrayList<ProjectCommit> projectCommits, int startIndex, int endIndex,
+            ArrayList<TestIdentifier> testIdentifiers) throws GitAPIException, IOException {
+
+        ArrayList<TestIdentifier> failedTests = testIdentifiers;
+        int i = endIndex;
+
+        for (; i >= startIndex; i -= batchSize) {
+            if (failedTests.size() == 0)
+                break;
+
+            int lastIndex = Math.min(i + batchSize, endIndex);
+
+            ArrayList<TestIdentifier> newFailedTests = new ArrayList<>();
+            ArrayList<TestIdentifier> batchTests = new ArrayList<>();
+
+            ArrayList<TestResult> testResults = projectInstance.runTestsForCommit(failedTests,projectCommits.get(i), projectCommits.get(lastIndex));
+
+            HashSet<TestIdentifier> newFailedTestsSet = TestResult.extractFailingTests(testResults);
+
+            for (TestIdentifier test : failedTests) {
+                if (newFailedTestsSet.contains(test))
+                    newFailedTests.add(test);
+                else
+                    batchTests.add(test);
+            }
+
+            super.runForCommitsAndTests(projectCommits, i + 1, Math.min(i + batchSize - 1, endIndex), batchTests);
+            
+            failedTests = newFailedTests;
+            if(failedTests.isEmpty())
+                break;
+        }
+
+        super.runForCommitsAndTests(projectCommits, startIndex, Math.min(i + batchSize - 1, endIndex), failedTests);
+    }
+}
