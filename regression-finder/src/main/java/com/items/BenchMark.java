9ddebc6483f54f3ef2c1eb05137c544de9d83055
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-10 18:46:26 +0530

    change time formatting

--- regression-finder/src/main/java/com/items/BenchMark.java
+++ regression-finder/src/main/java/com/items/BenchMark.java
@@ -4,6 +4,8 @@ import com.items.interfaces.CSVItem;
 
 public class BenchMark implements CSVItem {
 
+	private static final int SECONDS_PER_MINUTE = 60;
+	private static final int MILLISECONDS_PER_SECOND = 1000;
 	private long time;
 	private String method;
 
@@ -14,7 +16,15 @@ public class BenchMark implements CSVItem {
 
 	@Override
 	public String toCSVString() {
-        return this.method + "," + this.time;
+        return this.method + "," + getTime();
+	}
+
+	public String getTime(){
+		double seconds = time/MILLISECONDS_PER_SECOND;
+		long minutes = (long) Math.floor(seconds/SECONDS_PER_MINUTE);
+		seconds -= minutes*SECONDS_PER_MINUTE;
+
+		return minutes + "," + seconds;
 	}
     
 }
