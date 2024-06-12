6f440e8367758588604d37f936fdc302a6953c08
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-12 11:34:09 +0530

    add runTime logging

--- regression-finder/src/main/java/com/tool/Main.java
+++ regression-finder/src/main/java/com/tool/Main.java
@@ -21,7 +21,8 @@ public class Main {
         try {
             Helper.setup(repositoryPath, resultsPath);
             GitWorker.getRemoteRepository(repositoryPath, repositoryLink); 
-            RegressionTool.run(repositoryPath, "7.6.4", method, resultsPath, true);
+            long time = RegressionTool.run(repositoryPath, "7.6.4", method, resultsPath, true);
+            System.out.println("time: " + time + " ms");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
