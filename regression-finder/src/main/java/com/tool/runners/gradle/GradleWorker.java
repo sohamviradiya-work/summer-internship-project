8be773f8ab368b799dac8e6fedfe4d2087d01c2f
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-13 14:08:37 +0530

    correct gradle folder path

---
+++ regression-finder/src/main/java/com/tool/runners/gradle/GradleWorker.java
@@ -0,0 +1,113 @@
+package com.tool.runners.gradle;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.gradle.tooling.events.ProgressEvent;
+import org.gradle.tooling.events.test.TestOperationResult;
+import org.gradle.tooling.events.test.internal.DefaultJvmTestOperationDescriptor;
+import org.gradle.tooling.events.test.internal.DefaultTestFailureResult;
+import org.gradle.tooling.events.test.internal.DefaultTestFinishEvent;
+import org.gradle.tooling.events.test.internal.DefaultTestSkippedResult;
+
+import com.items.TestIdentifier;
+import com.items.TestResult;
+import com.tool.writers.ArrayListWriter;
+
+public class GradleWorker {
+    ProjectManager projectManager;
+
+    public GradleWorker(ProjectManager projectManager) {
+        this.projectManager = projectManager;
+    }
+
+    public void close() {
+        this.projectManager.close();
+    }
+
+    public ArrayList<TestResult> runTests(List<TestIdentifier> testIdentifiers) throws IOException {
+        HashMap<String, HashMap<String, List<String>>> testGroups = TestIdentifier.groupByProjectClass(testIdentifiers);
+        ArrayListWriter<TestResult> testResultsWriter = ArrayListWriter.create();
+        for (String testProject : testGroups.keySet()) {
+            testResultsWriter.writeAll(runTestsForProject(testProject, testGroups.get(testProject)));
+        }
+
+        return testResultsWriter.getList();
+    }
+
+    private ArrayList<TestResult> runTestsForProject(String testProjectName,
+            HashMap<String, List<String>> testMethods) {
+        ProjectTester projectTester = ProjectTester.mountProjectTester(projectManager);
+        ArrayList<ProgressEvent> events = projectTester.runTestsForProject(testProjectName, testMethods);
+        return extractResults(testProjectName, events);
+    }
+
+    private ArrayList<TestResult> runAlltestsForProject(String testProjectName) {
+        ProjectBuilder projectBuilder = ProjectBuilder.mountProjectBuilder(projectManager);
+        ArrayList<ProgressEvent> events = projectBuilder.runAlltestsForProject(testProjectName);
+        return extractResults(testProjectName, events);
+    }
+
+    public ArrayList<TestResult> runAllTests() throws IOException {
+
+        List<String> subProjects = projectManager.getSubProjects();
+        ArrayListWriter<TestResult> testResultsWriter = ArrayListWriter.create();
+        for (String testProjectName : subProjects) {
+            testResultsWriter.writeAll(runAlltestsForProject(testProjectName));
+        }
+
+        ArrayList<TestResult> testResults = testResultsWriter.getList();
+
+        return testResults;
+    }
+
+    public void syncDependencies() {
+        ProjectBuilder projectBuilder = ProjectBuilder.mountProjectBuilder(projectManager);
+        projectBuilder.syncDependencies();
+    }
+
+    public static GradleWorker mountGradleWorker(String gradleVersion, File directory) {
+        return new GradleWorker(ProjectManager.mountGradleProject(gradleVersion, directory));
+    }
+
+
+    private static ArrayList<TestResult> extractResults(String testProjectName,
+            ArrayList<ProgressEvent> events) {
+        ArrayList<TestResult> testResults = new ArrayList<>();
+        for (ProgressEvent event : events) {
+            if (event instanceof DefaultTestFinishEvent) {
+                TestResult testResult = GradleWorker.extractResult(event, testProjectName);
+                if (testResult != null)
+                    testResults.add(testResult);
+            }
+        }
+        return testResults;
+    }
+
+    public static TestResult extractResult(ProgressEvent event, String testProjectName) {
+        DefaultJvmTestOperationDescriptor descriptor = (DefaultJvmTestOperationDescriptor) event.getDescriptor();
+
+        TestOperationResult result = ((DefaultTestFinishEvent) event).getResult();
+        String resultString;
+
+        String testClassName = descriptor.getClassName();
+        String testMethodName = descriptor.getMethodName();
+
+        if (testClassName == null || testMethodName == null)
+            return null;
+
+        if (result instanceof DefaultTestFailureResult)
+            resultString = "FAILED";
+        else if (result instanceof DefaultTestSkippedResult)
+            resultString = "SKIPPED";
+        else
+            resultString = "PASSED";
+
+        testMethodName = testMethodName.replace("(", "").replace(")", "");
+
+        return new TestResult(testClassName, testMethodName, testProjectName, resultString);
+    }
+}
---
+++ regression-finder/src/main/java/com/tool/runners/gradle/ProjectBuilder.java
@@ -0,0 +1,54 @@
+package com.tool.runners.gradle;
+
+import java.util.ArrayList;
+
+import org.gradle.tooling.BuildLauncher;
+import org.gradle.tooling.events.OperationType;
+import org.gradle.tooling.events.ProgressEvent;
+import org.gradle.tooling.events.ProgressListener;
+
+public class ProjectBuilder {
+    private BuildLauncher buildLauncher;
+
+    private ProjectBuilder(BuildLauncher buildLauncher) {
+        this.buildLauncher = buildLauncher;
+    }
+
+    public static ProjectBuilder mountProjectBuilder(ProjectManager projectManager) {
+        BuildLauncher buildLauncher = projectManager.getConnection().newBuild();
+        return new ProjectBuilder(buildLauncher);
+    }
+
+    public void syncDependencies() {
+        buildLauncher.forTasks("dependencies");
+        buildLauncher.withArguments("--refresh-dependencies");
+
+        try {
+            System.out.println("Syncing Dependencies");
+            buildLauncher.run();
+        } catch (Exception e) {
+            
+        }
+    }
+
+    public ArrayList<ProgressEvent> runAlltestsForProject(String testProjectName) {
+        buildLauncher.forTasks(testProjectName + ":test");
+        buildLauncher.withArguments("--continue", "--quiet", "--parallel");
+        ArrayList<ProgressEvent> events = new ArrayList<>();
+
+        buildLauncher.addProgressListener(new ProgressListener() {
+
+            @Override
+            public void statusChanged(ProgressEvent event) {
+                events.add(event);
+            }
+        }, OperationType.TEST);
+
+        try {
+            buildLauncher.run();
+        } catch (Exception e) {
+
+        }
+        return events;
+    }
+}
