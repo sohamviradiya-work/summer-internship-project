20d0321f157c0093376142c158c3e1f1c13ef54c
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-17 15:46:07 +0530

    add jira ticket class

---
+++ regression-finder/src/main/java/com/items/interfaces/JiraItem.java
@@ -0,0 +1,5 @@
+package com.items.interfaces;
+
+public interface JiraItem {
+    public JiraTicket toJiraTicket();
+}
---
+++ regression-finder/src/main/java/com/items/interfaces/JiraTicket.java
@@ -0,0 +1,27 @@
+package com.items.interfaces;
+
+public class JiraTicket {
+    private String summary;
+    private String description;
+    private String email;
+
+    public JiraTicket(String summary, String description, String email) {
+        this.summary = summary;
+        this.description = description;
+        this.email = email;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
+
