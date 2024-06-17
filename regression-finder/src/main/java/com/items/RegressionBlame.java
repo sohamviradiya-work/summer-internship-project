bad79e5b5fc954ae2d3907150716f869d1b4feda
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-17 17:28:58 +0530

    add toJiraTicket method to regBlame

--- regression-finder/src/main/java/com/items/RegressionBlame.java
+++ regression-finder/src/main/java/com/items/RegressionBlame.java
@@ -1,8 +1,10 @@
 package com.items;
 
 import com.items.interfaces.CSVItem;
+import com.items.interfaces.JiraItem;
+import com.items.interfaces.JiraTicket;
 
-public class RegressionBlame implements CSVItem{
+public class RegressionBlame implements CSVItem, JiraItem {
 
     enum BlameType {
         TEST_FAIL,
@@ -13,14 +15,40 @@ public class RegressionBlame implements CSVItem{
     private TestIdentifier testIdentifier;
     private BlameType type;
 
-    public RegressionBlame(TestIdentifier testIdentifier, ProjectCommit projectCommit,boolean isTestFail) {
+    public RegressionBlame(TestIdentifier testIdentifier, ProjectCommit projectCommit, boolean isTestFail) {
         this.testIdentifier = testIdentifier;
         this.projectCommit = projectCommit;
         this.type = isTestFail ? BlameType.TEST_FAIL : BlameType.TEST_WRITE;
     }
-    
+
     @Override
     public String toCSVString() {
-        return testIdentifier.getTestProject() + ","+ testIdentifier.getTestClass() +  "," + testIdentifier.getTestMethod()+ "," + projectCommit.getCommitId() + "," + projectCommit.getAuthor() + "," + type.toString(); 
+        return testIdentifier.getTestProject() + "," + testIdentifier.getTestClass() + ","
+                + testIdentifier.getTestMethod() + "," + projectCommit.getCommitId() + "," + projectCommit.getAuthor()
+                + "," + type.toString();
+    }
+
+    @Override
+    public JiraTicket toJiraTicket() {
+
+        String description;
+
+        String summary = "Your commit " + this.projectCommit.getCommitId() + " at " + this.projectCommit.getDate()
+                + " failed some tests";
+
+        if (type == BlameType.TEST_WRITE) {
+
+            description = "Your commit " + this.projectCommit.getCommitId() + " changed the test "
+                    + this.testIdentifier.getTestProject() + ": "
+                    + this.testIdentifier.getTestClass() + "." + this.testIdentifier.getTestMethod()
+                    + " which has been failing since last phase";
+
+        } else {
+            description = "Your commit " + this.projectCommit.getCommitId() + " caused the test "
+                    + this.testIdentifier.getTestProject() + ":" + this.testIdentifier.getTestClass() + "."
+                    + this.testIdentifier.getTestMethod() + " to fail";
+        }
+
+        return new JiraTicket(summary, description, this.projectCommit.getAuthor());
     }
 }
